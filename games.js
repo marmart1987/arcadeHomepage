93855593bd8dfe720222556dc4ea20f784562baa
marmart1987 <marmtk1811@gmail.com>
2025-11-29 14:26:06 -0500

    games.js

---
+++ games.js
@@ -0,0 +1,6 @@
+const games = [
+    {title:"Pacman", thumbnail: "pacman.jpg", url: "https://example.com"}
+]
+console.log(document.querySelectorAll('*')+"1")
+const slots = document.getElementById("games").firstChild()
+console.log(slots)
