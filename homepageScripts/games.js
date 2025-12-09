fe6c17f422f98223c654a4de1901db840e99c3ed
marmart1987 <marmtk1811@gmail.com>
2025-12-09 17:04:47 -0500

    Cosmetic Changes

--- homepageScripts/games.js
+++ homepageScripts/games.js
@@ -1,5 +1,5 @@
 const games = [
-    { title: "Pacman", thumbnail: "pacman.jpg", thumbnailScale: 1.5, url: "/games/pacman/" },
+    { title: "Pacman", thumbnail: "pacman.jpg", thumbnailScale: 1.5,  url: "/games/pacman/"},
     { title: "Snake", thumbnail: "snake.jpg", thumbnailScale: 0.88, url: "https://example.com" }
 ]
 
@@ -11,7 +11,7 @@ for (let gameNo = 0; gameNo < numberOfSlots; gameNo++) {
     let gameElement = gameElements.item(gameNo)
 
 
-    gameElement.textContent = isDefined && games[gameNo].title ? games[gameNo].title : "Error"
+    gameElement.textContent = isDefined && games[gameNo].title ? games[gameNo].title : "___"
     gameElement.style["background-image"] = isDefined && games[gameNo].thumbnail ? `url(/assets/gameThumbnails/${games[gameNo].thumbnail})` : null
     gameElement.style["background-size"] = isDefined && games[gameNo].thumbnailScale ? `${games[gameNo].thumbnailScale * 100}%` : null
     gameElement.onclick = isDefined && games[gameNo].url ? (() => { window.location.href = games[gameNo].url; }) : null
